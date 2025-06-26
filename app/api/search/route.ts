05f7657eccc5e53435c54e703018982a75a88e38
Luke Taylor <luke.taylor@bbc.co.uk>
2025-06-26 14:34:04 +0100

    .

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,6 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
+export const dynamic = 'force-static';
+
 export const { GET } = createFromSource(source);
